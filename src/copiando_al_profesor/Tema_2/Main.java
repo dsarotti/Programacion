1e53a34bdda98fb70c3e4caee65fbcdc754d3303
dsarotti <sr.sarotti@gmail.com>
2022-10-14 13:27:04 +0200

    todo cool

--- src/copiando_al_profesor/Tema_2/Main.java
+++ src/copiando_al_profesor/Tema_2/Main.java
@@ -1,8 +1,12 @@
 package copiando_al_profesor.Tema_2;
 import java.util.Calendar;
 import java.util.Random;
+//Este paquete contiene clases, métodos y elementos probablemente inconexos y carentes de sentido para meros mortales como nosotros, obra de un genio incomprendido.
+//El IDE seguramente resaltará algunas variables
+
 class Main{
     public static void main(String[] args) {
+        char[] data = {'c','a','d','e','n','a'};
         String arg1 = args [0];
         String arg2 = args [1];
         int min = Integer.parseInt(arg1);
@@ -21,9 +25,7 @@ class Main{
 
         Calendar cal = Calendar.getInstance();
         cal.get(Calendar.MONTH);
-
         //System.out.println("El número mayor entre (%s y %d) es %d.%n",aleatorio1, aleatorio2, max_aleat);
-
     }
 }
 
---
+++ src/cosecha_propia/tema2/ejercicio1/Main.java
@@ -0,0 +1,14 @@
+import java.io.Console;
+public class Main {
+    public static void main(String[] args) {
+        clase objeto= new clase();
+       // this.palabra1= 
+    }
+    public void pide (){
+        Console c = System.console();
+       // this.palabra1=c.readLine();
+             
+    }
+    
+    
+}
