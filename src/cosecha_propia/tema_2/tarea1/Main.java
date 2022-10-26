f55f3b5e82cbbfbf08372a7dc9de5dfcd9773830
dsarotti <sr.sarotti@gmail.com>
2022-10-26 09:47:50 +0200

    asd

--- src/cosecha_propia/tema_2/tarea1/Main.java
+++ src/cosecha_propia/tema_2/tarea1/Main.java
@@ -9,58 +9,70 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         Console c = System.console();
         Palabras lista = new Palabras();
-        System.out.println("introduce una lista separada por comas: ");
+
+
+        System.out.println("introduce una lista de palabras separadas por comas: ");
         lista.introducir(c.readLine());
         int op;
 
+        //si se introduce una lista vacía se vuelve a pedir
         if (lista.getLista() == null){
             do {
-                System.out.println("la lista está vacía, prueba a llenarla orta vez.");
+                System.out.println("la lista está vacía, prueba a llenarla otra vez.");
                 lista.introducir(c.readLine());
             }while (lista.getLista() == null);
         }
-        System.out.println("lista introducida: ");
-        for(int i =0;i<lista.getLista().length;i++){
-            System.out.println(lista.getLista()[i]);
-        }
-        System.out.println("-------------------------");
-        
-            System.out.println("1) Filtrar.");
-            System.out.println("2) Dividir.");
-        do{    
-            op = pideInt(sc);
-            if (op<1 || op >2) System.out.println("Selecciona la opcion 1 o 2. ");
-        }while (op<1 || op >2);
+        do{
+            //Se muestra la lista introducida y el menú.
+            System.out.println("lista introducida: ");
+            for(int i =0;i<lista.getLista().length;i++){
+                System.out.println(lista.getLista()[i]);
+            }
+            System.out.println("-------------------------");
+            
+                System.out.println("1) Filtrar.");
+                System.out.println("2) Dividir.");
+                System.out.println("3) Salir");
+            //solo se admite el número 1, 2 o 3.
+            System.out.println("Selecciona la opcion 1, 2 o 3. ");
+            do{    
+                op = pideInt(sc);
+            }while (op<1 || op >3);
 
-        switch (op) {
-            case 1 :
-                System.out.print("introduce un filtro: ");
-                String filtro=c.readLine();
-                String[] temp = lista.filtrar(filtro);
-                System.out.println("Mostrando resultados filtrados por \"" + filtro + "\": ");
-                for(int i =0;i<temp.length;i++){
-                    System.out.println(temp[i]);
-                }
-                System.out.println("-------------------------");
-                break;
-            case 2 :
-                System.out.println("indica por qué palabra dividir. ");
-                String[][] dividir = lista.dividir(c.readLine());
 
-                System.out.println("primera parte de la lista: ");
-                System.out.println("----------");
-                for (int i=0;i<dividir[0].length;i++){
-                    System.out.println(dividir[0][i]);
-                }
-                System.out.println("----------");
+            switch (op) {
+                case 1 :
+                    System.out.print("introduce un filtro: ");
+                    String filtro=c.readLine();
+                    String[] temp = lista.filtrar(filtro);
+                    System.out.println("Mostrando resultados filtrados por \"" + filtro + "\": ");
+                    for(int i =0;i<temp.length;i++){
+                        System.out.println(temp[i]);
+                    }
+                    System.out.println("-------------------------");
+                    break;
+                case 2 :
+                    System.out.println("Indica por qué palabra dividir. ");
+                    String[][] dividir = lista.dividir(c.readLine());
 
-                System.out.println("segunda parte de la lista: ");
-                System.out.println("----------");
-                for (int i=0;i<dividir[1].length;i++){
-                    System.out.println(dividir[1][i]);
-                }
-                System.out.println("------------------");
-        }
+                    System.out.println("Primera parte de la lista: ");
+                    System.out.println("----------");
+                    //muestra la primera parte de la lista
+                    for (int i=0;i<dividir[0].length;i++){
+                        System.out.println(dividir[0][i]);
+                    }
+                    System.out.println("----------");
+
+                    System.out.println("Segunda parte de la lista: ");
+                    System.out.println("----------");
+                    //muestra la segunda parte de la lista
+                    for (int i=0;i<dividir[1].length;i++){
+                        System.out.println(dividir[1][i]);
+                    }
+                    System.out.println("-------------------------");
+            }
+        }while(op!=3);
+        System.out.println("Saliendo...");
     }
 
     public static int pideInt(Scanner sc){
@@ -68,7 +80,7 @@ public class Main {
         try {
             op=sc.nextInt();
         } catch (Exception e) {
-            System.out.println("elige 1 o 2. ");
+            System.out.println("Elige 1, 2 o 3. ");
         }
         return op;
     }
