179f5930a9bb1691def7de6276887c0b6983a744
dsarotti <sr.sarotti@gmail.com>
2022-10-26 08:23:40 +0200

    asd

--- src/cosecha_propia/tema_2/tarea1/Main.java
+++ src/cosecha_propia/tema_2/tarea1/Main.java
@@ -1,12 +1,74 @@
 package cosecha_propia.tema_2.tarea1;
 import java.io.Console;
+import java.util.Scanner;
+
+import javax.naming.event.NamingExceptionEvent;
 public class Main {
     public static void main(String[] args) {
-        Palabras lista = new Palabras();
+        Scanner sc = new Scanner(System.in);
         Console c = System.console();
-        System.out.println("introduce una lista separada por comas :");
+        Palabras lista = new Palabras();
+        System.out.println("introduce una lista separada por comas: ");
         lista.introducir(c.readLine());
+        int op;
+
+        if (lista.getLista() == null){
+            do {
+                System.out.println("la lista está vacía, prueba a llenarla orta vez.");
+                lista.introducir(c.readLine());
+            }while (lista.getLista() == null);
+        }
+        System.out.println("lista introducida: ");
+        for(int i =0;i<lista.getLista().length;i++){
+            System.out.println(lista.getLista()[i]);
+        }
+        System.out.println("-------------------------");
         
+            System.out.println("1) Filtrar.");
+            System.out.println("2) Dividir.");
+        do{    
+            op = pideInt();
+        }while (op<1 || op >2);
+
+        switch (op) {
+            case 1 :
+                System.out.print("introduce un filtro: ");
+                String filtro=c.readLine();
+                String[] temp = lista.filtrar(filtro);
+                System.out.println("Mostrando resultados filtrados por \"" + filtro + "\": ");
+                for(int i =0;i<temp.length;i++){
+                    System.out.println(temp[i]);
+                }
+                System.out.println("-------------------------");
+                break;
+            case 2 :
+                System.out.println("indica por qué palabra dividir. ");
+                String[][] dividir = lista.dividir(c.readLine());
+
+                System.out.println("primera parte de la lista: ");
+                System.out.println("----------");
+                for (int i=0;i<dividir[0].length;i++){
+                    System.out.println(dividir[0][i]);
+                }
+                System.out.println("----------");
+
+                System.out.println("segunda parte de la lista: ");
+                System.out.println("----------");
+                for (int i=0;i<dividir[1].length;i++){
+                    System.out.println(dividir[1][i]);
+                }
+                System.out.println("------------------");
+        }
     }
-    
+
+    public static int pideInt(){
+        Scanner sc = new Scanner(System.in);
+        int op=-1;
+        try {
+            op=sc.nextInt();
+        } catch (Exception e) {
+            System.out.println("elige 1 o 2. ");
+        }
+        return op;
+    }
 }
--- src/cosecha_propia/tema_2/tarea1/Palabras.java
+++ src/cosecha_propia/tema_2/tarea1/Palabras.java
@@ -1,38 +1,84 @@
 package cosecha_propia.tema_2.tarea1;
-//import java.util.Arrays;
+import java.io.Console;
+import java.util.Arrays;
 
 public class Palabras {
-    String[] lista;
+    private String[] lista=null;
+
+    public String[] getLista(){
+        return this.lista;
+    }
 
     public void introducir(String entrada){
-        this.lista = new String[entrada.split(", ").length]; //genera el array de tamaño fijo.
-        this.lista = entrada.split(", ");//divide la lista por comas y la guarda en el array creado antes.
-        for (int i = 0 ;i < this.lista.length;i++){ //quita posibles espacios en blanco antes y después de cada palabra.
-            this.lista[i]=this.lista[i].strip();
-        }    
+        if (entrada.split(", ").length>0){
+            this.lista = new String[entrada.split(", ").length]; //genera el array de tamaño fijo.
+            this.lista = entrada.split(", ");                    //divide la lista por comas y la guarda en el array creado antes.
+            for (int i = 0 ;i < this.lista.length;i++){                 
+                this.lista[i]=this.lista[i].strip();                    //quita posibles espacios en blanco antes y después de cada palabra.
+            }
+            if(this.lista.length==1){                                   //si sólo se introducen palabras vacías, se deja la lista como null.
+                if(this.lista[0].isBlank()) this.lista = null;
+            }
+        }
     }
 
     public String[] filtrar(String filtro){
+        /* 
+         * Filtra la lista de este objeto y devuelve la nueva lista con las palabras que cumplan el filtro.
+         * si el parámetro filtro esta vacío se devuelve una lista vacía.
+         */
         String res="";
-        char tempChars[];
-        char filtroChars[]=filtro.toCharArray();//pasamos el filtro a array de chars para comparar.
-        boolean trigger;
-        for (int i = 0;i<this.lista.length;i++){ //recorremos todas las palabras.
-            trigger=false;
-            tempChars=this.lista[i].toCharArray(); //alojamos temporalmente cada palabra en un array de Chars.
-            
-            if ((filtroChars.length<=tempChars.length) && (tempChars[0] == filtroChars[0]) ){   //si el filtro cabe en la palabra y
-                trigger=true; 
-                for(int n = 1 ;(n < filtroChars.length) || (!trigger);n++){//recorremos la palabra comparando con el filtro. 
-                    if (tempChars[n]!=filtroChars[n]){    //si en algun momento el filtro no concuerda se pasa trigger a false.
-                        trigger=false;              //lo que provoca que se pare el bucle para esa palabra y no se añada a la lista.
+        if (this.lista!=null && !(filtro.isEmpty())){
+            char[] tempChars;
+            char[] filtroChars=filtro.toCharArray();//pasamos el filtro a array de chars para comparar.
+            boolean sw; //lo utilizaremos para controlar si se encuentra el patrón.
+            for (int i = 0;i<this.lista.length;i++){ //recorremos todas las palabras.
+                sw=false;
+                tempChars=this.lista[i].toCharArray(); //alojamos temporalmente la palabra en un array de Chars.
+                
+                if ((filtroChars.length<=tempChars.length) && (tempChars[0] == filtroChars[0])){//si el filtro cabe en la palabra y la primera letra concuerda.
+                    sw=true;
+                    for(int n = 1 ;(n < filtroChars.length) && (sw);n++){//recorremos la palabra comparando con el filtro. 
+                        if (tempChars[n]!=filtroChars[n]){  //si en algun momento el filtro no concuerda se cambia sw a false.
+                            sw=false;                       //lo que provoca que se pare el bucle para esa palabra y no se añada a la lista.
+                        }
                     }
+                    if (sw) res=res+","+this.lista[i]; //si al recorrer la palabra sw sigue siendo true, el filtro concuerda, se añade la palabra.
+                    tempChars.toString();
                 }
-                if (trigger){
-                res=res+","+tempChars.toString();
-                }
-            }
+            }  
         }
         return res.split(",");
+    }
+
+    public String[][] dividir(String divisor){
+        /* 
+         * ordena la lista de este objeto y luego la divide por la primera .
+         * ocurrencia del divisor que recibe como parámetro de entrada
+         * devuelve un array de dos dimensiones con dos posiciones en la primera
+         * dimensión y las que sean necesarias en la segunda dimensión
+         */
+        String[] temp = this.lista.clone();             //clonamos para no modificar el array original.
+        Arrays.sort(temp);                              //ordenamos la copia.
+        int clave = Arrays.binarySearch(temp, divisor); //obtenemos el índice por el que dividir el array.
+        Console c = System.console();
+
+        if (clave<0) do {
+            System.out.println("esa palabra no existe en la lista, prueba con otra: ");
+            clave = Arrays.binarySearch(temp, c.readLine());
+        }while (clave <0); 
+        
+        String[][] res = new String[2][];
+        res[0] = new String[clave];
+        res[1] = new String[temp.length - clave];
+
+        for (int i = 0;i<temp.length;i++){
+            if (i<clave){
+                res[0][i]=temp[i];
+            }else{
+                res[1][i-clave]=temp[i];
+            }
+        }
+        return res;
     }
 }
