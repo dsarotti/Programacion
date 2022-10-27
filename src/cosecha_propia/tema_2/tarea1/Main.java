dcf981fe296b856e9f80707727d53e6496540f1e
dsarotti <sr.sarotti@gmail.com>
2022-10-27 17:56:11 +0200

    google-style

--- src/cosecha_propia/tema_2/tarea1/Main.java
+++ src/cosecha_propia/tema_2/tarea1/Main.java
@@ -1,4 +1,5 @@
 package cosecha_propia.tema_2.tarea1;
+
 import java.io.Console;
 import java.util.Scanner;
 
@@ -11,75 +12,73 @@ public class Main {
         Console c = System.console();
         Palabras lista = new Palabras();
 
-
         System.out.println("introduce una lista de palabras separadas por comas: ");
         lista.introducir(c.readLine());
         int op;
 
-        //si se introduce una lista vacía se vuelve a pedir
-        if (lista.getLista() == null){
+        // si se introduce una lista vacía se vuelve a pedir
+        if (lista.getLista() == null) {
             do {
                 System.out.println("la lista está vacía, prueba a llenarla otra vez.");
                 lista.introducir(c.readLine());
-            }while (lista.getLista() == null);
+            } while (lista.getLista() == null);
         }
-        do{
-            //Se muestra la lista introducida y el menú.
+        do {
+            // Se muestra la lista introducida y el menú.
             System.out.println("lista introducida: ");
-            for(int i =0;i<lista.getLista().length;i++){
+            for (int i = 0; i < lista.getLista().length; i++) {
                 System.out.println(lista.getLista()[i]);
             }
             System.out.println("-------------------------");
-            
-                System.out.println("1) Filtrar.");
-                System.out.println("2) Dividir.");
-                System.out.println("3) Salir");
-            //solo se admite el número 1, 2 o 3.
-            System.out.println("Selecciona la opcion 1, 2 o 3. ");
-            do{    
-                op = pideInt(sc);
-            }while (op<1 || op >3);
 
+            System.out.println("1) Filtrar.");
+            System.out.println("2) Dividir.");
+            System.out.println("3) Salir");
+            // solo se admite el número 1, 2 o 3.
+            System.out.println("Selecciona la opcion 1, 2 o 3. ");
+            do {
+                op = pideInt(sc);
+            } while (op < 1 || op > 3);
 
             switch (op) {
-                case 1 :
+                case 1:
                     System.out.print("introduce un filtro: ");
-                    String filtro=c.readLine();
+                    String filtro = c.readLine();
                     String[] temp = lista.filtrar(filtro);
                     System.out.println("Mostrando resultados filtrados por \"" + filtro + "\": ");
-                    for(int i =0;i<temp.length;i++){
+                    for (int i = 0; i < temp.length; i++) {
                         System.out.println(temp[i]);
                     }
                     System.out.println("-------------------------");
                     break;
-                case 2 :
+                case 2:
                     System.out.println("Indica por qué palabra dividir. ");
                     String[][] dividir = lista.dividir(c.readLine());
 
                     System.out.println("Primera parte de la lista: ");
                     System.out.println("----------");
-                    //muestra la primera parte de la lista
-                    for (int i=0;i<dividir[0].length;i++){
+                    // muestra la primera parte de la lista
+                    for (int i = 0; i < dividir[0].length; i++) {
                         System.out.println(dividir[0][i]);
                     }
                     System.out.println("----------");
 
                     System.out.println("Segunda parte de la lista: ");
                     System.out.println("----------");
-                    //muestra la segunda parte de la lista
-                    for (int i=0;i<dividir[1].length;i++){
+                    // muestra la segunda parte de la lista
+                    for (int i = 0; i < dividir[1].length; i++) {
                         System.out.println(dividir[1][i]);
                     }
                     System.out.println("-------------------------");
             }
-        }while(op!=3);
+        } while (op != 3);
         System.out.println("Saliendo...");
     }
 
-    public static int pideInt(Scanner sc){
-        int op=-1;
+    public static int pideInt(Scanner sc) {
+        int op = -1;
         try {
-            op=sc.nextInt();
+            op = sc.nextInt();
         } catch (Exception e) {
             System.out.println("Elige 1, 2 o 3. ");
         }
--- src/old/Fibonaci.java
+++ src/old/Fibonaci.java
@@ -1,12 +1,13 @@
 package old;
-public class Fibonaci{
-    //TERMINADO
-    public static void main(String[] args){
+
+public class Fibonaci {
+    // TERMINADO
+    public static void main(String[] args) {
         int n = 0;
-        for (int i = 1;i < 9999;i = (n+i)){ 
+        for (int i = 1; i < 9999; i = (n + i)) {
             System.out.println(n);
             System.out.println(i);
-            n=(n+i);
+            n = (n + i);
         }
     }
 }
