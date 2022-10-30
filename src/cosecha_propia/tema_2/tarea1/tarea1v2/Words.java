44ef4d228b3f3f16f835afcbf7a89575be06f257
dsarotti <sr.sarotti@gmail.com>
2022-10-30 14:06:49 +0100

    tarea1

--- src/cosecha_propia/tema_2/tarea1/tarea1v2/Words.java
+++ src/cosecha_propia/tema_2/tarea1/tarea1v2/Words.java
@@ -10,17 +10,16 @@ public class Words {
      * De esta forma el código queda más simplificado y compacto.
      */
     public static void main(String[] args) {
+
         /*
          * Muestra un menú por la salida estándar y pide que se introduzca una opción
          * por teclado. Después ejecuta la opción solicitada.
          */
-
-        Console c = System.console();
         String[] words;
         int op;
 
         System.out.print("Introduce palabras separadas por \", \": ");
-        words = fill(c);
+        words = fill();
 
         do {
             System.out.println("############-Menu-############");
@@ -28,23 +27,24 @@ public class Words {
             System.out.println("Opcion 2: Dividir y ordenar. #");
             System.out.println("Opcion 3: Salir.             #");
             System.out.println("##############################");
-            op = selectOption(c);
+            op = selectOption();
             switch (op) {
                 case 1:
-                    filter(words, c);
+                    filter(words);
                     break;
                 case 2:
-                    divide(words, c);
+                    divide(words);
                     break;
             }
         } while (op != 3);
     }
 
-    public static String[] fill(Console c) {
+    public static String[] fill() {
         /*
          * Pide una String de palabras separadas por ", " y devuelve una Array con una
          * palabra en cada posición. Como mínimo admite una palabra.
          */
+        Console c = System.console();
         String[] res;
         String resStr;
         resStr = c.readLine();
@@ -59,10 +59,11 @@ public class Words {
         return res;
     }
 
-    public static int selectOption(Console c) {
+    public static int selectOption() {
         /*
          * Pide al usuario que introduzca una opción. Sólo admite números del 1 al 3.
          */
+        Console c = System.console();
         int temp = -1;
 
         do {
@@ -76,30 +77,32 @@ public class Words {
         return temp;
     }
 
-    private static void filter(String[] words, Console c) {
+    private static void filter(String[] words) {
         /**
          * Recibe una Array de Strings @param words y pide al usuario que introduzca un
          * filtro.
          * Busca palabras que empiecen por dicho filtro dentro del array y las muestra.
          */
+        Console c = System.console();
         String filter;
         System.out.print("Introduce algo por lo que filtrar: ");
         do {
             filter = c.readLine();
         } while (filter.isEmpty() || filter.isBlank());
         for (int i = 0; i < words.length; i++) {
-            if (words[i].indexOf(filter) == 0) {
+            if (words[i].startsWith(filter)) {
                 System.out.println(words[i]);
             }
         }
     }
 
-    private static void divide(String[] words, Console c) {
+    private static void divide(String[] words) {
         /**
          * Recibe un Array de Strings. Solicita al usuario que introduzca una cadena.
          * Ordena el array lexicográficamente y muestra las palabras anteriores y
          * posteriores o iguales a la cadena antes solicitada.
          */
+        Console c = System.console();
         String[] ordered = words.clone();
         Arrays.sort(ordered);
         String div;
